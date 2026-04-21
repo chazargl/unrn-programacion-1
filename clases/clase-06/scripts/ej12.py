37145322eba88583ed448e1092011fca7bdbecc9
Juan Matias Chazarreta <73183200+chazargl@users.noreply.github.com>
2026-04-21 01:09:57 -0300

    Add files via upload

---
+++ clases/clase-06/scripts/ej12.py
@@ -0,0 +1,19 @@
+comienzo = True
+seleccion_usuario = []
+while comienzo :
+    producto = input('Ingrese su eleccion o "fin" para terminar. ')
+    if producto == 'fin' :
+        print('\nSe ingreso fin.')
+        break
+    else :
+        seleccion_usuario.append(producto)
+
+def mostrar_productos_unicos (seleccion) :
+    productos_unicos = []
+    for sel in seleccion :
+        if sel not in productos_unicos :
+            productos_unicos.append(sel)
+    print(f'\nLa cantidad de productos ingresados fue: {len(seleccion)}.')
+    print(f'\nLos productos elegidos fueron: {productos_unicos}')
+
+mostrar_productos_unicos (seleccion_usuario)
