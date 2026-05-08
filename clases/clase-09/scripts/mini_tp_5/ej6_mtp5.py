d49a9a45aff983d45b07a12d83e7dc0cc39ffb35
Juan Matias Chazarreta <73183200+chazargl@users.noreply.github.com>
2026-05-08 13:38:58 -0300

    Update ej6_mtp5.py

--- clases/clase-09/scripts/mini_tp_5/ej6_mtp5.py
+++ clases/clase-09/scripts/mini_tp_5/ej6_mtp5.py
@@ -3,10 +3,16 @@ producto = {'nombre': 'mouse', 'precio': 12500, 'stock': 6}
 # Recorro e imprimo claves.
 for clave in producto.keys() :
     print(clave)
+# Resolucion sin el method()
+# for clave in producto :
+#    print(clave)
 
 # Recorro e imprimo valores.
 for valor in producto.values() :
     print(valor)
+# Resolucion sin el method()
+# for valor in producto :
+#    print(producto[valor])
 
 # Recorro e imprimo claves y valores.
 for clave, valor in producto.items() :
